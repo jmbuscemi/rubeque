898e74f724b9b144c752d502d52ae023ae9e4233
John M Buscemi <jmbuscemi@gmail.com>
2015-09-25 11:06:49 -0400

    Add comments to Easy puzzles

--- easy.rb
+++ easy.rb
@@ -1,7 +1,9 @@
+#Reverse Each Word
 def reverse_each_word(sentence)
   sentence.split.map{|w| w.reverse}.join(" ")
 end
 
+#Class Test
 class Foo
   def name
     "foo"
@@ -18,11 +20,13 @@ class Bar < Foo
   end
 end
 
+#Your Favorite and Mine, Fibonacci!
 def fibo_finder(n)
   return n if (n < 2)
   fibo_finder(n-1) + fibo_finder(n-2)
 end
 
+#The Little Hash Key that Could
 def key_for_min_value(hash)
   hash.key(hash.values.min)
 end
